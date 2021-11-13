d88725dc0773f259a5a586b818883ec7300d9e12
Arnulfo Rodriguez <arnulfo.rodriguez@gmail.com>
2021-11-12 20:31:13 -0600

    Neat solution for Absolute Permutation

--- src/AbsolutePermutation.hs
+++ src/AbsolutePermutation.hs
@@ -2,28 +2,12 @@ module AbsolutePermutation where
 
 
 import Data.Sequence as Seq
-import Data.Maybe as Maybe
 import Data.Foldable as Foldable
+import Control.Monad
   
-possible k n = map (\ i -> (if abs i <= k then Nothing else Just (abs (k - i)), if (k + i) <= n then Just (k+i) else Nothing) ) [1..n]
-
-absolutePermutation 0 n = [1..n]
-absolutePermutation k n  =
-  let possibleValues = possible k n
-      absolutePermutationRec [] currentAbsolutePermutation = Just currentAbsolutePermutation
-      absolutePermutationRec (possiblePair:remainingPairs) currentAbsolutePermutation = 
-         case possiblePair of
-              (Nothing , Just x) -> if Maybe.isNothing (x `Seq.elemIndexL` currentAbsolutePermutation) then absolutePermutationRec remainingPairs (currentAbsolutePermutation |> x) else Nothing
-              (Just x, Nothing) ->  if Maybe.isNothing (x `Seq.elemIndexL` currentAbsolutePermutation) then absolutePermutationRec remainingPairs (currentAbsolutePermutation |> x) else Nothing
-              (Just x, Just y) ->  if Maybe.isNothing (x `Seq.elemIndexL` currentAbsolutePermutation) 
-                                       then 
-                                         case absolutePermutationRec remainingPairs (currentAbsolutePermutation |> x) of
-                                           Nothing -> if Maybe.isNothing (y `Seq.elemIndexL` currentAbsolutePermutation) then absolutePermutationRec remainingPairs (currentAbsolutePermutation |> y) else Nothing
-                                           result -> result
-                                       else if Maybe.isNothing (y `Seq.elemIndexL` currentAbsolutePermutation)
-                                            then  absolutePermutationRec remainingPairs (currentAbsolutePermutation |> y)
-                                            else Nothing
-              _ -> Nothing
-  in case absolutePermutationRec possibleValues Seq.empty of
-       (Just l) -> Foldable.toList l
-       Nothing -> [(-1)]                               
+absolutePermutation n 0 = [1..n]
+absolutePermutation n k
+ | (n `mod` (k * 2)) /= 0 = [-1]
+ | otherwise = let chunks = Seq.chunksOf (2*k) (Seq.fromList [1..n])
+                   reverseSubChunk _ chunk =  Control.Monad.join (Seq.reverse (Seq.chunksOf k chunk))
+               in  Foldable.toList $  Control.Monad.join (Seq.mapWithIndex reverseSubChunk chunks)
