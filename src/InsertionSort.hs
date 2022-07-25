72428796c30e4c4fdec7a3ff4a5499afa43d9b95
Arnulfo Rodriguez <arnulfo.rodriguez@gmail.com>
2022-07-24 20:16:05 -0600

    simpler implementation

--- src/InsertionSort.hs
+++ src/InsertionSort.hs
@@ -7,12 +7,18 @@ import Data.List as List
 import Data.Traversable as Traversable
 import Data.Foldable as Foldable
   
-merge :: Seq Int -> Seq Int -> (Int, Seq Int) -> (Int, Seq Int)
-merge Seq.Empty b (currentCount, currentMerge) = (currentCount + Seq.length b * Seq.length currentMerge, currentMerge)
-merge a Seq.Empty (currentCount, currentMerge) = (currentCount, currentMerge >< a)
-merge left@(leftHead :<| leftTail) right@(rightHead :<| rightTail) (currentCount, currentMerge)
- | leftHead <= rightHead = merge leftTail right (currentCount, currentMerge |> leftHead)
- | otherwise = merge left rightTail (currentCount + Seq.length left, currentMerge |> rightHead)
+merge :: Seq Int -> Seq Int -> (Int, Seq Int)
+merge Seq.Empty b = (0, b)
+merge a Seq.Empty = (0, a)
+merge left@(leftHead :<| leftTail) right@(rightHead :<| rightTail) 
+ | leftHead <= rightHead = 
+   let
+    (count,merged)  = merge leftTail right
+   in (count, leftHead <| merged)
+ | otherwise =
+   let 
+    (count, merged) = merge left rightTail                             
+   in (count + Seq.length left, rightHead <| merged)
      
 mergeSort :: Seq Int -> (Int, Seq Int)
 mergeSort Seq.Empty = (0, Seq.empty)
@@ -23,7 +29,7 @@ mergeSort theSeq =
     (left,right) = Seq.splitAt middle theSeq
     (leftCount, leftSorted) = mergeSort left
     (rightCount, rightSorted) =  mergeSort right
-    (mergeCount, merged) =  merge leftSorted rightSorted (0, Seq.empty)
+    (mergeCount, merged) =  merge leftSorted rightSorted
   in (rightCount + leftCount + mergeCount, merged)
 
 insertionSort arr =
