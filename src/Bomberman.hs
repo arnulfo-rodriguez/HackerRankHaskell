9a3486559c427925f775a8a150f1b5dbb35920e2
Arnulfo Rodriguez <arrodriguez@microsoft.com>
2024-12-15 07:15:08 -0600

    Latest submission

--- src/Bomberman.hs
+++ src/Bomberman.hs
@@ -13,7 +13,8 @@ import System.Environment
 import System.IO
 import System.IO.Unsafe
 import Text.Printf (printf)
-import Data.Sequence as Seq
+import Data.Array as Array
+import Data.Array (array, assocs, bounds, indices, elems, (!))
 import Data.Foldable
 
 --
@@ -24,66 +25,63 @@ import Data.Foldable
 --  1. INTEGER n
 --  2. STRING_ARRAY grid
 --
-data Cell = Planted Int | EmptyCell
-data Matrix = Matrix Int Int (Seq (Seq Cell))
-data BombermanStates = DoNothing | Plant
+data Cell = Planted Int Int Int | EmptyCell Int Int
+data Matrix = Matrix Int Int (Array Int Cell)
 
+indexToPosition :: Int -> Int -> (Int -> Int -> a) -> a
+indexToPosition columns index  constructor =
+    let row = index `div` columns
+        col = index `mod` columns
+    in (constructor row col)
+    
+fromString '.' columns index = indexToPosition columns index EmptyCell
+fromString 'O' columns index  = indexToPosition columns index (Planted 0)
 
-fromString '.' = EmptyCell
-fromString 'O' = Planted 0
-
-toString EmptyCell = '.'
-toString (Planted _) = 'O'
-
-nextState DoNothing = Plant
-nextState Plant = DoNothing
-
-
-
-mapNested f = fmap (fmap f)
+toString (EmptyCell _ _) = '.'
+toString (Planted _ _ _) = 'O'
 
 stringMatrixToGrid :: Int -> Int -> [String] -> Matrix
-stringMatrixToGrid rows cols matrix = Matrix rows cols $  Seq.fromList $  Data.List.map Seq.fromList $ mapNested fromString matrix
+stringMatrixToGrid rows cols matrix = 
+  let
+    flattenedMatrix = Data.List.concat matrix
+    theArray = array (0 , (rows * cols) - 1) [(i, fromString x cols i) | (i,x) <- Data.List.zip [0..] flattenedMatrix]
+  in Matrix rows cols theArray
 
-
+toRows cols rows arr = [[arr ! (row * cols + col) | col <- [0..cols-1]] | row <- [0..rows-1]]
+                   
 gridToStringMatrix:: Matrix -> [String]
-gridToStringMatrix (Matrix _ _ theData) =   Data.Foldable.toList $  (Data.Foldable.toList <$> mapNested toString theData)
+gridToStringMatrix (Matrix rows cols theData) =  toRows cols rows $ fmap toString theData
 
 plantCell :: Int -> Cell -> Cell
-plantCell ticks EmptyCell = Planted ticks
-plantCell _ p@(Planted _) = p
+plantCell ticks (EmptyCell i j) = Planted ticks i j
+plantCell _ p@(Planted _ _ _) = p
 
 plantGrid :: Int -> Matrix -> Matrix
-plantGrid ticks (Matrix x y theData) = Matrix x y (mapNested (plantCell ticks) theData)
+plantGrid ticks (Matrix x y theData) = Matrix x y (fmap (plantCell ticks) theData)
 
 explodeGrid :: Int -> Matrix -> Matrix
 explodeGrid ticks matrix@(Matrix x y theData) =
     let
-        mapRow :: Int -> Seq Cell -> Seq (Int, Int, Cell)
-        mapRow i = Seq.mapWithIndex (\ j cell -> (i, j, cell))
-        mappedMatrix =  Data.Foldable.foldl (\ l row ->  Data.Foldable.toList row ++ l) [] $ Seq.mapWithIndex mapRow theData
-        willExplode (Planted t) = (ticks - t) == 3
+        willExplode (Planted t _ _) = (ticks - t) == 3
         willExplode _  = False
-        toExplode = Data.List.filter (\ (_,_,c) -> willExplode c) mappedMatrix
+        toExplode = Data.List.filter willExplode (Data.Array.elems theData)
     in
-        Data.Foldable.foldl  (\ m (i, j, cell) -> explodeCell i j cell ticks m)  matrix toExplode
-
+        Data.Foldable.foldl  (\ m cell -> explodeCell cell ticks m) matrix toExplode
 
 replaceCell x y newCell m@(Matrix rows cols theData)
  | x < 0 || x >= rows || y < 0 || y >= cols = m
  | otherwise =
     let
-        theRow = Seq.index theData x
-        newRow = Seq.update y newCell theRow
-        newTheData = Seq.update x newRow theData
+        thePosition = (x * cols) + y
+        newTheData =  theData // [(thePosition, newCell)]
     in  Matrix rows cols newTheData
 
 
 cellAndNeighbors x y (Matrix rows cols _) =  (x,y):Data.List.filter (\ (i,j) -> i >= 0 && i < rows && j >= 0  && j < cols)  [(x - 1, y), (x + 1, y), (x , y - 1) , (x, y + 1)]
 
-explodeCell :: Int -> Int -> Cell -> Int -> Matrix -> Matrix
-explodeCell _ _ EmptyCell _ grid = grid
-explodeCell x y p@(Planted plantedTime) ticks matrix@(Matrix rows cols _)  = Data.List.foldl (\ accMatrix (i,j) -> replaceCell i j EmptyCell accMatrix) matrix (cellAndNeighbors x y matrix)
+explodeCell :: Cell -> Int -> Matrix -> Matrix
+explodeCell (EmptyCell _ _) _ grid = grid
+explodeCell p@(Planted plantedTime x y) ticks matrix@(Matrix rows cols _)  = Data.List.foldl (\ accMatrix (i,j) -> replaceCell i j (EmptyCell i j) accMatrix) matrix (cellAndNeighbors x y matrix)
 
 iterateBomber g ticks
   | even ticks = plantGrid ticks g
@@ -126,6 +124,6 @@ bombermanMain = do
     let result = bomberMan n r c grid
 
     hPutStrLn fptr $ Data.List.intercalate "\n" result
-
+  
     hFlush fptr
     hClose fptr
